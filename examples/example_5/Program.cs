dcc481da23413a0bd3d67bb13ad1338576ea3788
Ivan Simonov <inculto@yandex.ru>
2023-06-20 16:06:40 +0300

    added example_5

---
+++ examples/example_5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Пожалуйста, введите своё имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+	Console.WriteLine("О нет, только не Маша");
+}
+else
+{
+	Console.WriteLine("Привет, " + username);
+}
