f7977124308e12fbb09c455c639e611222d722ee
Ivan Simonov <inculto@yandex.ru>
2023-06-20 16:07:38 +0300

    some changes in example_3

--- examples/example_3/Program.cs
+++ examples/example_3/Program.cs
@@ -1,5 +1,7 @@
-﻿int number_fst = 3;
-int number_snd = 5;
+﻿int number_fst = new Random().Next(1, 11); // 1,2,3,4..10
+Console.WriteLine(number_fst);
+int number_snd = new Random().Next(1, 11);
+Console.WriteLine(number_snd);
 int sum = number_fst + number_snd;
 
 Console.WriteLine(sum);
