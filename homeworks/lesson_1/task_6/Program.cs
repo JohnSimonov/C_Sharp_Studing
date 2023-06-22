2d9928794b7ae3e65977c2df0741c9a7dafaebba
Ivan Simonov <inculto@yandex.ru>
2023-06-22 22:52:34 +0300

    added task_6 of lesson_1 homework

---
+++ homeworks/lesson_1/task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+	Console.WriteLine($"Number {number} is even");
+}
+else
+{
+	Console.WriteLine($"Number {number} is odd");
+}
+
