ff9ce30a5ac3c2ad507ab5f499449e397899986f
Ivan Simonov <inculto@yandex.ru>
2023-06-22 22:35:26 +0300

    added homework lesson_1 task_1

---
+++ homeworks/lesson_1/task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter first number: ");
+int first_number = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int second_number = int.Parse(Console.ReadLine());
+
+if (first_number > second_number)
+{
+	Console.WriteLine($"max = {first_number}");
+}
+else
+{
+	Console.WriteLine($"max = {second_number}");
+}
