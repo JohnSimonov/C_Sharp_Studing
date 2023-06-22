453643e01d40e292f67b0277c9f52adafd29d3a2
Ivan Simonov <inculto@yandex.ru>
2023-06-22 23:12:09 +0300

    added task_8 of lesson_1 homework

---
+++ homeworks/lesson_1/task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number N: ");
+int number_n = int.Parse(Console.ReadLine());
+
+int even_number = 2;
+
+while (even_number <= number_n )
+{
+	Console.Write($"{even_number} ");
+	even_number += 2;
+}
+Console.WriteLine("");
